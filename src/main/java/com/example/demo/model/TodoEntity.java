cb1a6b65a7386b472a7d1cce888e3540243d2c58
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-10-24 21:04:41 +0900

    2-2 백엔드 서비스 아키텍처 -  2.2.5 서비스 레이어 - 비즈니스 로직 / 2.2.6 퍼시스턴스 레이어: 스프링 데이터 JPA

--- src/main/java/com/example/demo/model/TodoEntity.java
+++ src/main/java/com/example/demo/model/TodoEntity.java
@@ -4,12 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity // 자바 클래스를 엔티티로 지정하려면 이 어노테이션 필요. 이름도 부여할 수 있다. ex) @Entity("TodoEntity")
+@Table(name = "Todo")   // 테이블 이름 지정. 데이터베이스의 Todo테이블에 매핑된다는 뜻. 지정을 하지 않는 경우 클래스의 이름을 테이블 이름으로 간주.
 public class TodoEntity {
+    @Id // 기본키가 될 필드에 지정
+    @GeneratedValue(generator = "system-uuid")  // ID 자동 생성
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
     private String id;      // 이 오브젝트의 아이디
     private String userId;  // 이 오브젝트를 생성한 사용자의 아이디
     private String title;   // Todo 타이틀(예: 운동하기)
---
+++ src/main/java/com/example/demo/persistance/TodoRepository.java
@@ -0,0 +1,19 @@
+package com.example.demo.persistance;
+
+import com.example.demo.model.TodoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, String> {
+    // 스프링 데이터 JPA가 메서드 이름을 파싱해서
+    // SELECT * FROM TodoRepository WHERE userId = '{userId}'와 같은 쿼리를 작성해 실행
+    // 메서드 이름은 쿼리, 매개변수는 쿼리의 where 문에 들어갈 값을 의미
+    // List<TodoEntity> findByUserId(String userId);
+
+    @Query("select * from Todo t where t.userId = ?1")
+    List<TodoEntity> findByuserId(String userId);
+}
